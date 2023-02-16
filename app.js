234d361c707dbd7e518a4e160dad31b73293e298
Kaz Walls <124752720+akhwalls@users.noreply.github.com>
2023-02-15 20:59:47 -0700

    Mobile nav toggle and assorted cleaning tasks (#1)
    * [chore] move js to own file and add doctype (test commit)
    
    * [chore] deminify css for prod + minor clean + move gfonts to links
    
    * [fix] reenable mobile toggle
    
    * [chore] more minor css cleans - collapse dupe media rules + remove unused wp presets
    
    * [feat] link app.js in other pages, add margin to body for fixed nav
    
    * Revert "[chore] more minor css cleans - collapse dupe media rules + remove unused wp presets"
    
    This reverts commit 4f12f5c823a8a0b02eef5a536a79dd729b5d687f.
    
    * [chore] redo clean unused wp presets
    
    * [fix] fix a few style regressions

---
+++ app.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+    if ($(window).width() >= 767) {
+        $('.navbar-nav > li.menu-item > a').click(function () {
+            window.location = $(this).attr('href');
+        });
+    }
+
+    $('.mobile-toggle').on('click', function () {
+        $('.collapse').toggleClass('open');
+    });
+});

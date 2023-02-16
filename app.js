9cb39e44eac7b740888f23a8edf0e73755e6bf85
AKH Walls <akhwalls@protonmail.com>
2023-02-15 20:16:08 -0700

    [fix] reenable mobile toggle

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-jQuery( document ).ready( function( $ ) {
-    if ( $( window ).width() >= 767 ) {
-        $( '.navbar-nav > li.menu-item > a' ).click( function() {
-            window.location = $( this ).attr( 'href' );
-        } );
+jQuery(document).ready(function ($) {
+    if ($(window).width() >= 767) {
+        $('.navbar-nav > li.menu-item > a').click(function () {
+            window.location = $(this).attr('href');
+        });
     }
-} );
+
+    if ($(window).width() <= 991) {
+        $('.mobile-toggle').on('click', function () {
+            $('.collapse').toggleClass('open');
+        });
+    }
+});

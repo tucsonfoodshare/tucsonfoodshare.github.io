734c3f7f43fb9fdb91715657be171d92de9b0d7b
Joshua Mack <mackncheesiest@gmail.com>
2023-04-03 09:05:57 -0700

    Adjusted regex, made grocery times display larger

--- homepage.js
+++ homepage.js
@@ -3,8 +3,11 @@
 const post_announcements = ($) => {
     const header = "Special header value leave me here";
     const footer = "Special footer value leave me here";
+    // Real
     const url = "https://docs.google.com/document/d/1oua98OaFsRKr2h1O6xs-y2wMqJDusQmetHB-0A7YIU8/edit?usp=sharing";
-    const regex = new RegExp(`${header}(\\\\n)+(?<announcements>[\(\)a-zA-Z0-9].*[\(\)a-zA-Z0-9])(\\\\n)+${footer}`);
+    // Testing
+    //const url = "https://docs.google.com/document/d/1bDakt0aOl-E5TvjI8oC5Zf3u7gKi5gfhDYMGMfc3bnw/edit?usp=sharing";
+    const regex = new RegExp(`${header}(\\\\n)+(?<announcements>[\!\(\)a-zA-Z0-9].*[\!\(\)a-zA-Z0-9])(\\\\n)+${footer}`);
 
     fetch(url)
       .then((response) => response.text())
@@ -15,7 +18,6 @@ const post_announcements = ($) => {
         } else {
             return "";
         }
-        //return text.match(regex).groups['announcements'].trim();
       })
       .then((announcements) => {
         if (announcements) {

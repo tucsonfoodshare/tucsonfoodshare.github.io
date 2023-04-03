b1ddea4423b6b3b5177caf14bcb806c8e861b19c
Joshua Mack <mackncheesiest@gmail.com>
2023-04-03 09:32:49 -0700

    Force desktop site of google docs when on mobile
    The layout/structure of the mobilebasic site it otherwise loads
    completely doesn't work with the current regex setup and would likely
    need actual parsing of the HTML. Might be better long-term to use that
    approach on both desktop and mobile (might give a more resilient parsing
    approach, etc), but for now we're just forcing desktop instead
    
    Also, we need to show <br> tags on mobile for page-breaks to render
    properly in this announcements area

--- homepage.js
+++ homepage.js
@@ -4,9 +4,9 @@ const post_announcements = ($) => {
     const header = "Special header value leave me here";
     const footer = "Special footer value leave me here";
     // Real
-    const url = "https://docs.google.com/document/d/1oua98OaFsRKr2h1O6xs-y2wMqJDusQmetHB-0A7YIU8/edit?usp=sharing";
+    const url = "https://docs.google.com/document/d/1oua98OaFsRKr2h1O6xs-y2wMqJDusQmetHB-0A7YIU8/edit?overridemobile=true";
     // Testing
-    //const url = "https://docs.google.com/document/d/1bDakt0aOl-E5TvjI8oC5Zf3u7gKi5gfhDYMGMfc3bnw/edit?usp=sharing";
+    //const url = "https://docs.google.com/document/d/1bDakt0aOl-E5TvjI8oC5Zf3u7gKi5gfhDYMGMfc3bnw/edit?overridemobile=true";
     const regex = new RegExp(`${header}(\\\\n)+(?<announcements>[\!\(\)a-zA-Z0-9].*[\!\(\)a-zA-Z0-9])(\\\\n)+${footer}`);
 
     fetch(url)

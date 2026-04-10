6b1549c5312d45ed7bc768527bfc865e2bcaec79
Joshua Mack <mackncheesiest@gmail.com>
2026-04-09 22:34:31 -0700

    Updated CSS & removed dead js

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,6 @@
 "use strict";
 
 jQuery(document).ready(function ($) {
-    if ($(window).width() >= 767) {
-        $('.navbar-nav > li.menu-item > a').click(function () {
-            window.location = $(this).attr('href');
-        });
-    }
-
     $('.mobile-toggle').on('click', function () {
         var expanded = $(this).attr('aria-expanded') === 'true';
         $(this).attr('aria-expanded', !expanded);
